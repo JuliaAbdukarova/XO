eeb1124ba5794cd77f382fa4eb98b0101874a081
JuliaAbdukarova <juliannas@list.ru>
2023-07-26 21:10:43 +0300

    redux

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,24 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Board from "./Board";
-import style from "./Game.module.css";
+import style from "./css/Game.module.css";
+import { appStore } from "../store";
+import { connect } from "react-redux";
 
 const Game = () => {
-  const [cells, setCells] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+  // const [cells, setCells] = useState(Array(9).fill(null));
+  //const [xIsNext, setXIsNext] = useState(true);
 
   const handleClick = (i) => {
-    const newCells = [...cells];
-    if (calculateWinner(cells) || newCells[i]) {
+    //const newCells = [...cells];
+    const newCells = [...appStore.getState().cells];
+
+    //if (calculateWinner(cells) || newCells[i]) {
+    if (calculateWinner(appStore.getState().cells) || newCells[i]) {
       return;
     }
-    newCells[i] = xIsNext ? "X" : "O";
-    setCells(newCells);
-    setXIsNext(!xIsNext);
+    //console.log(`appStore.getState().xIsNext = ${appStore.getState().xIsNext}`);
+    newCells[i] = appStore.getState().xIsNext ? "X" : "O";
+
+    //setCells(newCells);
+    appStore.dispatch({
+      type: "move",
+      payload: { cells: newCells, xIsNext: appStore.getState().xIsNext },
+    });
+
+    //setXIsNext(!xIsNext);
   };
 
   const handleRestart = () => {
-    setCells(Array(9).fill(null));
-    setXIsNext(true);
+    //setCells(Array(9).fill(null));
+    appStore.dispatch({ type: "restart", payload: {} });
+    //setXIsNext(true);
   };
 
   const calculateWinner = (cells) => {
@@ -41,18 +54,25 @@ const Game = () => {
     return null;
   };
 
-  const winner = calculateWinner(cells);
+  //const winner = calculateWinner(cells);
+  const winner = calculateWinner(appStore.getState().cells);
   const status = winner
     ? `Победитель: ${winner}`
-    : `Следующий  игрок: ${xIsNext ? "X" : "O"}`;
-
+    : `Следующий  игрок: ${appStore.getState().xIsNext ? "X" : "O"}`;
+  // console.log(appStore.getState());
   return (
     <div className={style.game}>
       <div>{status}</div>
-      <Board cells={cells} onClick={handleClick} />
+      <Board cells={appStore.getState().cells} onClick={handleClick} />
       {winner && <button onClick={handleRestart}>Начать заново</button>}
     </div>
   );
 };
 
-export default Game;
+//export default Game;
+
+const mapStateToProps = (state) => ({
+  ...state,
+});
+
+export default connect(mapStateToProps)(Game);
